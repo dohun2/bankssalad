c1bac9fa78a35952981a476bc837460f654451ed
Dohun2 <dohun1221@icloud.com>
2021-08-01 23:42:39 +0900

    Add calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,7 +1,11 @@
-const date = new Date();
+let date = new Date();
+
+const renderCalendar = () => {
 
 const year = date.getFullYear();
 const month = date.getMonth();
+
+document.querySelector('.month-text').textContent = `${year}년 ${month + 1}월`;
 
 const pastMonth = new Date(year, month, 0);
 const nowMonth = new Date(year, month + 1, 0);
@@ -28,8 +32,17 @@ for (let i = 1; i < 7 - nowMonthDay; i++) {
 
 const dates = pastMonthDates.concat(nowMonthDates, futureMonthDates);
 
-dates.forEach((date, i) => {
-  dates[i] = `<div class="date">${date}</div>`;
-})
+const firstDateIndex = dates.indexOf(1);
+  const lastDateIndex = dates.lastIndexOf(nowMonthDate);
+  dates.forEach((date, i) => {
+    const condition = i >= firstDateIndex && i < lastDateIndex + 1
+                      ? 'this'
+                      : 'other';
+
+    dates[i] = `<div class="date"><span class="${condition}">${date}</span></div>`;
+  })
 
 document.querySelector('.dates').innerHTML = dates.join('');
+}
+
+renderCalendar();
