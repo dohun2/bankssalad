541daaecb8e0641d678109f4394c224cde2f28ba
Dohun2 <dohun1221@icloud.com>
2021-08-02 20:55:10 +0900

    Add go page

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function goMenu() {
+  window.location.href = '/main-list.html'
+}
+function goList() {
+  window.location.href = '/main-list.html'
+}
+function goCalendar() {
+  window.location.href = '/main-calendar.html'
+}
+function goStat() {
+  window.location.href = '/main-statistics.html'
+}
+function goLogin() {
+  window.location.href = '/index.html'
+}
